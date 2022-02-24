26b9c5dc6814ac3f5342e72f546431e97db6b6b4
mara <mara@the-witch.ru>
2022-02-24 19:17:39 +0300

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
 export default {
   root: "./dist",
   build: {
     outDir: "./",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './dist/index.html'),
+        art1: resolve(__dirname, './dist/articles/izbytochnye-nazvaniya-klassov/index.html'),
+        art2: resolve(__dirname, './dist/articles//pochemu-ya-rabotayu-tolko-s-laravel/index.html'),
+      }
+    }
   }
 }
