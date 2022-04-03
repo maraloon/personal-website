34aa2107031a2c84b10e10f70ab3d9ff2abd08e6
mara <mara@the-witch.ru>
2022-04-03 15:23:25 +0300

    typefix

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default {
         main: resolve(__dirname, './dist/index.html'),
         art1: resolve(__dirname, './dist/articles/izbytochnye-nazvaniya-klassov/index.html'),
         art2: resolve(__dirname, './dist/articles/pochemu-ya-rabotayu-tolko-s-laravel/index.html'),
-        art2: resolve(__dirname, './dist/articles/zarplata/index.html'),
+        art3: resolve(__dirname, './dist/articles/zarplata/index.html'),
       }
     }
   }
